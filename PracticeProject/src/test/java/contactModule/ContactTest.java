ac6ff275e17ceb4f7e304f5b0aa74c0fccd9507d
lenovo <lenovo@LAPTOP-IL8SBD7O>
2024-05-21 03:49:19 -0700

    Comitted1

--- PracticeProject/src/test/java/contactModule/ContactTest.java
+++ PracticeProject/src/test/java/contactModule/ContactTest.java
@@ -6,14 +6,10 @@ public class ContactTest {
 	@Test
 	public void createContactTest() {
 		System.out.println("execute create contact test");
-		String URL= System.getProperty("url");
-		String BROWSER= System.getProperty("browser");
-		String USERNAME= System.getProperty("username");
-		String PASSWORD= System.getProperty("password");
-		System.out.println(URL);
-		System.out.println(BROWSER);
-		System.out.println(USERNAME);
-		System.out.println(PASSWORD);
+		System.out.println("URL");
+		System.out.println("BROWSER");
+		System.out.println("USERNAME");
+		System.out.println("PASSWORD");
 	}
 
 	@Test
